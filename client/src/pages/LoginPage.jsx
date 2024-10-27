1e465bea5cac1e7aebc5975c0c10dbed29f07ac5
Tests <michalisouroumis@gmail.com>
2024-10-27 15:30:05 +0100

    feat: handle login in front end

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+import * as authService from "../services/authService";
+
 export function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const data = await authService.login(email, password);
+
+    if (data.errors) {
+      setMessage(data.errors[0].msg);
+      return;
+    }
+    setMessage(data.message);
+  };
+
   return (
     <div className="login-screen-container">
       <div>
         <h1>Login Here</h1>
-        <form>
-          <input placeholder="email" />
-          <input placeholder="password" />
+        <form onSubmit={handleLogin}>
+          <input
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button type="submit">Login</button>
         </form>
         <p>
           Not registered yet? <a href="/register">click here to register</a>
         </p>
+        <p>{message}</p>
       </div>
     </div>
   );
--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -8,3 +8,25 @@ export async function registerUser(email, password) {
   });
   return await response.json();
 }
+
+export async function login(email, password) {
+  try {
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Login error:", error);
+  }
+}
