4fc3b2cc77f2d12f779c8daf9df2a330ec2556f5
Tests <michalisouroumis@gmail.com>
2024-10-27 15:32:18 +0100

    chore: change error to message in authController

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -18,6 +18,6 @@ exports.login = async (req, res) => {
     res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.error("Failed to login", error);
-    res.status(500).json({ error: "Error logging in" });
+    res.status(500).json({ message: "Error logging in" });
   }
 };
