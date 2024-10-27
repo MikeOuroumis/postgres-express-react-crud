0f4585875833cd5617ec8db98b01232bad74d4e8
Tests <michalisouroumis@gmail.com>
2024-10-27 08:44:31 +0100

    feat: add validation for user registration

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const authController = require("../controllers/authController");
+const { valiidateRegistration } = require("../middlewares/authMiddleware");
 
 const router = Router();
 
-router.post("/register", authController.registerUser);
+router.post("/register", valiidateRegistration, authController.registerUser);
 
 module.exports = router;
