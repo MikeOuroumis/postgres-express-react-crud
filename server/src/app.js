89f0d51e37559e3d12f31fe1592e7bf717c3fc22
Tests <michalisouroumis@gmail.com>
2024-11-03 05:06:56 +0100

    feat(logger): added logger request middleware

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,16 @@ const cors = require("cors");
 const app = express();
 const hotelRoutes = require("./routes/hotelRoutes");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const requestLogger = require("./middlewares/requestLogger");
 
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(requestLogger);
 
+// Route Handlers
 app.use(hotelRoutes);
 app.use(authRoutes);
 
